0af80a562fad9ed2d547ec22ba615ca962d9254e
YoshikazuHosono <yyhhwork@gmail.com>
2022-02-17 19:48:05 +0900

    add crush sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,6 +20,18 @@ class MyApp extends StatelessWidget {
   }
 }
 
+final soundList = [
+  "C.wav",
+  "D.wav",
+  "E.wav",
+  "F.wav",
+  "G.wav",
+  "A.wav",
+  "B.wav",
+];
+
+Timer? crushTimer;
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
@@ -24,6 +40,31 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("audio_players_sample"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  var audioCache = AudioCache();
+                  crushTimer = Timer.periodic(const Duration(milliseconds: 200), (timer) {
+                    final idx = Random().nextInt(soundList.length);
+                    audioCache.play(soundList[idx]);
+                  });
+                }, 
+                child: const Text("play crush sound")
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  crushTimer?.cancel();
+                },
+                child: const Text("stop crush sound")
+            ),
+            ElevatedButton(onPressed: () {}, child: const Text("play no crush sound")),
+            ElevatedButton(onPressed: () {}, child: const Text("stop no crush sound")),
+          ],
+        ),
       ),
     );
   }
